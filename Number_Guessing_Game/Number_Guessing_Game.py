6f35e6119931d2549d76c64d5ef9c244b224c6b4
Siddhesh Patel <70743244+SIDDHESH-cyber@users.noreply.github.com>
2022-08-06 23:25:37 +0530

    Update Number_Guessing_Game.py

--- Number_Guessing_Game/Number_Guessing_Game.py
+++ Number_Guessing_Game/Number_Guessing_Game.py
@@ -1,1 +1,32 @@
+import random
+import sys
 
+def game(n, u):
+ num = 1
+ while u != n:
+  if(n > u):
+    u = int(input("Write A Higher Number Please : "))
+  elif(n < u):
+    u = int(input("Write A Lower Number Please : "))
+  else:
+     print("You Got Right Answer.")
+  num = num + 1
+ return num
+
+
+def main():
+    n = random.randint(1, 100)
+    # print(n)
+    u = int(input("Enter A Number And Guess The Correct Answer : "))
+    s = game(n, u)
+    print(f"You Got The Answer Correct In : {s}")
+
+    re = input("Do You Want To Play This Game Again (Press Y or y) : ")
+    if re == "y" or re == "Y":
+     main()
+    else:
+     sys.exit(0)
+
+
+if __name__ == "__main__":
+ main()
